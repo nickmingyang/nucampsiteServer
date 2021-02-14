48d19a17a37c188bc09d69b74a70b0640cc043ff
nickmingyang <nickmingyang@gmail.com>
2021-02-13 21:17:20 -0500

    update git files prevent secret

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  secretKey: "12345-67890-09876-54321",
-  mongoUrl: "mongodb://localhost:27017/nucampsite",
-  facebook: {
-    clientId: "565124367789175",
-    clientSecret: "5142fa370be4fd3a640a97927845f077",
-  },
-};
